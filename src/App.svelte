3be91bf462082e5b21e85ec3cfbf1726cb2c4571
Vhyrro <vhyrro@gmail.com>
2021-08-27 15:00:14 +0200

    feat: make our app actually functional

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<main>
+	<h1>Hello!</h1>
+	<p>This is a test just to make sure everything works</p>
+</main>
+<br>
+<br>
+
+<h2>
+	We have different font sizes too!
+</h2>
+
+<p>Taurivim will hopefull be awesome</p>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
